1f758add48446ba3b51ee57dd31642c17b44347c
Joseph <josephstevenespinozaoviedo@gmail.com>
2023-06-05 14:33:50 -0500

    hola

---
+++ app/src/main/java/com/example/hola_mundo_joseph/Principal.kt
@@ -0,0 +1,12 @@
+package com.example.hola_mundo_joseph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Principal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
